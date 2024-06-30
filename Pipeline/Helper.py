4487cd7d918550c85f2820be0aca3137092b01d6
Arnau <1630311@uab.cat>
2024-06-30 17:59:51 +0200

    acabant

--- Pipeline/Helper.py
+++ Pipeline/Helper.py
@@ -63,29 +63,35 @@ def separate_labels(train_set, test_set):
 
 
 '''
-Function to separate both the labels and the classes of the data, also dropping the unknowns.
+Function to separate both the labels and the classes of the data.
 Useful for training the reconstruction error for example, where you need to distinguish by class.
 '''
 def split_labels_classes(train_set, test_set):
     # Split train data in licit and ilicit
     X_ilicit = train_set.loc[train_set['class'].isin([0])] 
     X_licit = train_set.loc[train_set['class'].isin([1])] 
+    X_unknown = train_set.loc[train_set['class'].isin([2])] 
     X_ilicit = X_ilicit.drop(columns=['class'])
     X_licit = X_licit.drop(columns=['class'])
+    X_unknown = X_unknown.drop(columns=['class'])
 
     y_ilicit = list(train_set.loc[train_set['class'].isin([0])]['class'])
     y_licit = list(train_set.loc[train_set['class'].isin([1])]['class'])
+    y_unknown = list(train_set.loc[train_set['class'].isin([2])]['class'])
     
 
     test_ilicit_X = test_set.loc[test_set['class'].isin([0])] 
     test_licit_X = test_set.loc[test_set['class'].isin([1])] 
+    test_unknown_X = test_set.loc[test_set['class'].isin([2])] 
     test_ilicit_X = test_ilicit_X.drop(columns=['class'])
     test_licit_X = test_licit_X.drop(columns=['class'])
+    test_unknown_X = test_unknown_X.drop(columns=['class'])
 
     test_ilicit_y = list(test_set.loc[test_set['class'].isin([0])]['class'])
     test_licit_y = list(test_set.loc[test_set['class'].isin([1])]['class'])
+    test_unknown_y = list(test_set.loc[test_set['class'].isin([2])]['class'])
 
-    return X_ilicit, y_ilicit, X_licit, y_licit, test_ilicit_X, test_ilicit_y, test_licit_X, test_licit_y 
+    return X_ilicit, y_ilicit, X_licit, y_licit, X_unknown, y_unknown, test_ilicit_X, test_ilicit_y, test_licit_X, test_licit_y, test_unknown_X, test_unknown_y 
 
 
 '''
